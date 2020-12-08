61464f3c34a62206f7df009c6381f237f0cbdb18
NanciNunes <nanci.nunes@ymail.com>
2020-12-08 17:18:59 -0300

    planner

---
+++ semana4/planner/index.js
@@ -0,0 +1,21 @@
+/* data: 08/12/20 planner
+
+1 - Crie uma função que é chamada ao clicar no botão "Criar Tarefa". 
+Use um evento apropriado que identifique o clique no botão.
+
+2 - Dentro da função, capture o valor do campo do HTML (que é a tarefa a ser colocada no planner) 
+e armazene em uma variável.
+
+3 - Capture o valor do seletor do HTML (o dia da semana selecionado) e armazene em uma variável.
+
+4 - Dê um `console.log` das variáveis para checar se você pegou os valores corretamente 
+(só não esqueça de **não** subir o código com `console.log`).
+
+5 - Adicione, através do JS, a tarefa (capturada no passo 2) na `div` identificada com o dia da semana que você capturou no passo 3.
+
+6 - Limpe o campo da tarefa. Você deve acessar o elemento do HTML da mesma forma que você acessou para capturar seu valor e 
+trocar para que não apareça nada no seu valor.
+
+
+sorry, não entendi. vou assistir a aula novamente - embora tenho a sensação de estar muito atrasada pois não estou assimilando 
+como os demais - e tentar entender e fazer exercícios*/
